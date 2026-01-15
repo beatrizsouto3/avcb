b7acd21e20a0064f6ed18236a50346f3a3309d27
Bia Souto <absouto.silva@gmail.com>
2026-01-15 15:53:19 -0300

    Listagem de registros

--- sistema.php
+++ sistema.php
@@ -1,5 +1,8 @@
 <?php
     session_start();
+
+    include_once('config.php');
+
     // print_r($_SESSION);
 
     if((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true)){
@@ -7,7 +10,13 @@
         unset($_SESSION['email']);
         unset($_SESSION['senha']);
     }
-    $logado = $_SESSION['email']
+    $logado = $_SESSION['email'];
+
+    $sql = "SELECT * FROM usuarios ORDER BY id DESC";
+
+    $stmt = $pdo->prepare($sql);
+
+    $stmt->execute();
 
 ?>
 
@@ -23,6 +32,10 @@
             background-image: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
             text-align: center;
             color: white;
+        }
+        .table-bg{
+            background: rgba(0,0,0,0.3);
+            border-radius: 15px 15px 0 0;
         }
     </style>
 </head>
@@ -41,6 +54,46 @@
     <br>
   <?php
     echo "<h1>Bem vindo <u>$logado</u></h1>";
-  ?> 
+  ?>
+    <br>
+  <div class="m-5">
+    <table class="table text-white table-bg">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Email</th>
+                <th scope="col">Senha</th>
+                <th scope="col">Telefone</th>
+                <th scope="col">Sexo</th>
+                <th scope="col">Data de Nascimento</th>
+                <th scope="col">Cidade</th>
+                <th scope="col">Estado</th>
+                <th scope="col">Endereço</th>
+                <th scope="col">...</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                while($user_data = $stmt->fetch(PDO::FETCH_ASSOC)){
+                    echo "<tr>";
+                    echo "<td>" . $user_data['id'] . "</td>";
+                    echo "<td>" . $user_data['nome'] . "</td>";
+                    echo "<td>" . $user_data['email'] . "</td>";
+                    echo "<td>" . $user_data['senha'] . "</td>";
+                    echo "<td>" . $user_data['telefone'] . "</td>";
+                    echo "<td>" . $user_data['sexo'] . "</td>";
+                    echo "<td>" . $user_data['data_nascimento'] . "</td>";
+                    echo "<td>" . $user_data['cidade'] . "</td>";
+                    echo "<td>" . $user_data['estado'] . "</td>";
+                    echo "<td>" . $user_data['endereco'] . "</td>";
+                    echo "<td>Ações</td>";
+                    echo "</tr>";
+                }
+            ?>
+        </tbody>
+    </table>
+  </div>
+
 </body>
 </html>
